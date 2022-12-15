a0a6fd9c80aa330147acba50a5fc89345d29b451
Scott Stafford <scott.stafford@databricks.com>
2022-12-15 14:31:36 -0500

    Add some more unit tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -5,3 +5,9 @@ from validation.address import *
 def test_zip_code_validation():
     valid_5_digits_zip = "22207"
     assert is_zip_code_valid(valid_5_digits_zip) == True
+    invalid_5_digits_zip = "2A207"
+    assert is_zip_code_valid(invalid_5_digits_zip) == False
+    invalid_5_digits_zip = "22207-1234"
+    assert is_zip_code_valid(invalid_5_digits_zip) == True
+    invalid_5_digits_zip = "22207-12345"
+    assert is_zip_code_valid(invalid_5_digits_zip) == False
