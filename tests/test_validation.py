574521d202b96c7155cc9cfc5e01e5e8b3e45534
Scott Stafford <scott.stafford@databricks.com>
2022-12-16 09:36:41 -0500

    Break out valid vs invalid tests

--- tests/test_validation.py
+++ tests/test_validation.py
@@ -2,18 +2,25 @@ import pytest
 
 from validation.address import *
 
-def test_zip_code_format_validation():
+def test_zip_code_is_valid():
     valid_5_digits_zip = "22207"
+    assert is_zip_code_format_valid(valid_5_digits_zip) == True  
+    valid_5_digits_zip = "22207-1234"
     assert is_zip_code_format_valid(valid_5_digits_zip) == True
+    
+
+def test_zip_code_is_not_valid():
     invalid_5_digits_zip = "2A207"
     assert is_zip_code_format_valid(invalid_5_digits_zip) == False
-    invalid_5_digits_zip = "22207-1234"
-    assert is_zip_code_format_valid(invalid_5_digits_zip) == True
     invalid_5_digits_zip = "22207-12345"
     assert is_zip_code_format_valid(invalid_5_digits_zip) == False
 
-def test_city_zip_code_pair_valid():
+def test_city_zip_code_pair_is_valid():
     zip = "22033"
     city = "Fairfax"
-    assert is_city_zip_code_pair_valid(city, zip) == True
+    assert is_city_zip_code_pair_valid(city, zip) == True  
 
+def test_city_zip_code_pair_is_not_valid():
+    zip = "22033-12"
+    city = "Fairfax"
+    assert is_city_zip_code_pair_valid(city, zip) == False
--- validation/address.py
+++ validation/address.py
@@ -4,6 +4,16 @@ from pyspark.sql import DataFrame
 
 spark = SparkSession.builder.getOrCreate()
 
+def is_city_zip_code_pair_valid(city: str, zip:str) -> bool:
+    if (not is_zip_code_format_valid(zip)):
+        return False
+    df = get_zip_code_city_lookup()
+    df = df.select("*").where((df.ZipCode == zip) & (df.City == city))
+    if df.count() > 0:
+        return True
+    else: 
+        return False
+
 def is_zip_code_format_valid(zip: str) -> bool:
     zip_code_regex = "(^\\d{5}$)|(^\\d{9}$)|(^\\d{5}-\\d{4}$)"
     pattern = re.compile(zip_code_regex)
@@ -12,16 +22,8 @@ def is_zip_code_format_valid(zip: str) -> bool:
     else:
         return False
 
-def is_city_zip_code_pair_valid(city: str, zip:str) -> bool:
-    df = get_zip_code_city_lookup()
-    df = df.select("*").where((df.ZipCode == zip) & (df.City == city))
-    if df.count() > 0:
-        return True
-    else: 
-        return False
-
 def get_zip_code_city_lookup() -> DataFrame:
     schema = ["City", "State", "ZipCode"]
     data = [ ("Fairfax", "VA", "22033")]
-    zip_code_city_df = spark.createDataFrame(data, schema)
+    zip_code_city_df = spark.createDataFrame(data, schema).cache()
     return zip_code_city_df
